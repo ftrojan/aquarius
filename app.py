62bb5d376906d50745e05666be0f69bcb10abe7f
filip_trojan <Tarantula2018>
2020-04-24 15:44:33 +0200

    flask with Bokeh TapTool implemented

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from dfply import X, mask
 from flask import Flask, render_template, url_for
 from bokeh.embed import components
 from bokeh.resources import INLINE
+from bokeh.models import OpenURL, TapTool
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -16,20 +17,40 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 app = Flask(__name__)
+js_resources = INLINE.render_js()
+css_resources = INLINE.render_css()
 logging.debug("logging works")
 current_year = utils.get_current_year()
 stations = utils.load_stations()
 totals = pd.read_csv('../../data/yearly_totals/prcp_totals.csv')
+logging.debug(f"{len(totals)} totals loaded")
 engine = utils.sql_engine()
 drought = utils.get_drought(engine).join(stations)
+logging.debug(f"{len(drought)} stations with drought information loaded from {engine}")
 
 
 @app.route('/')
 def index():
-    cz = stations >> mask(X.country_name == 'Czech Republic')
+    cz = drought >> mask(X.country_name == 'Czech Republic')
+    plotdf = utils.drought_add_facecolor(cz)
+    f_drought = utils.drought_rate_plot(plotdf)
+    url = "/station_detail/station_id=@station"
+    # taptool = f_drought.select(type=TapTool)
+    # taptool.callback = OpenURL(url=url)
+    f_drought.add_tools(TapTool(callback=OpenURL(url=url)))
+    bokeh_map = utils.drought_map(plotdf)
+    bokeh_map.add_tools(TapTool(callback=OpenURL(url=url)))
+    script_drought, div_drought = components(f_drought)
+    script_map, div_map = components(bokeh_map)
     return render_template(
         'main_page.html',
         stations=cz,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        script_drought=script_drought,
+        script_map=script_map,
+        div_drought=div_drought,
+        div_map=div_map,
     )
 
 
@@ -44,8 +65,6 @@ def station_detail(station_id: str):
     dft = totals.loc[totals['station'] == station_id, :]
     f_prcp = utils.cum_prcp_plot(stlabel, rdf, cprcp, curr_drought_rate)
     f_totals = utils.totals_barchart(dft)
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
     script, div = components(f_prcp)
     script_totals, div_totals = components(f_totals)
     html = render_template(
